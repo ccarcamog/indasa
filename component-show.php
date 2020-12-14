ab8a4d4972893ed89ac306a23b199e77802b78a9
Luis Mario Ramirez Peralta <luismarioram99@gmail.com>
2020-12-14 12:52:20 -0600

    Solucionado fadein de carousel y creada clase de invertido

--- component-show.php
+++ component-show.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,14 +9,20 @@
 	<link rel="stylesheet" href="/css/componentes.css">
 	<link rel="stylesheet" href="/css/bootstrap/bootstrap.min.css">
 </head>
+
 <body>
-	<?php include "componentes/quienes-somos.php" ?>
-	<style>
-		.svg-invert{
-			filter: invert(100%);
-		}
-	</style>
-	<script src="/js/jquery/jquery.min.js"></script>
-	<script src="/js/bootstrap/bootstrap.min.js"></script>
+	<?php // include "componentes/quienes-somos.php" ?>
+
+	<!-- Jquery-2.2.4 js -->
+	<script src="js/jquery/jquery.min.js"></script>
+	<!-- Popper js -->
+	<script src="js/bootstrap/popper.min.js"></script>
+	<!-- Bootstrap-4 js -->
+	<script src="js/bootstrap/bootstrap.min.js"></script>
+	<!-- All Plugins js -->
+	<script src="js/others/plugins.js"></script>
+	<!-- Active JS -->
+	<script src="js/active.js"></script>
 </body>
+
 </html>
--- componentes/quienes-somos.php
+++ componentes/quienes-somos.php
@@ -33,46 +33,12 @@
 
 		<div id="carouselCaracteristicas" class="carousel slide" data-ride="carousel">
 
-			<div class="carousel-inner">
-				<div class="carousel-item">
-					<div class="row">
-						<!-- Single Feature -->
-						<div class="col-12 col-md">
-							<div class="fplus-single-feature wow fadeInUp" data-wow-delay="1s">
-								<div class="feature-title d-flex align-items-center">
-									<img src="img/icons/wallet.svg" alt="">
-									<h5>Calidad garantizada</h5>
-								</div>
-								<p>Todos nuestros servicios cumplen con normas y estándares de calidad que garitizan la satisfacción de nuestro clientes</p>
-							</div>
-						</div>
-						<!-- Single Feature -->
-						<div class="col-12 col-md">
-							<div class="fplus-single-feature wow fadeInUp" data-wow-delay="1.5s">
-								<div class="feature-title d-flex align-items-center">
-									<img src="img/icons/credit-card.svg" alt="">
-									<h5>Precios razonables</h5>
-								</div>
-								<p>Planes de pago que se adaptan a cualquier presupuesto, y a cualquier tipo de empresa. Trabajamos bajo sus condiciones</p>
-							</div>
-						</div>
-						<!-- Single Feature -->
-						<div class="col-12 col-md">
-							<div class="fplus-single-feature wow fadeInUp" data-wow-delay="2s">
-								<div class="feature-title d-flex align-items-center">
-									<img src="img/icons/switching-user.svg" alt="">
-									<h5>Soporte contínuo</h5>
-								</div>
-								<p>Nuestro equipo está preparado para atender las necesidades más puntuales. Nuestro servicio fideliza a nuestro clientes</p>
-							</div>
-						</div>
-					</div>
-				</div>
+			<div class="carousel-inner wow fadeInUp" data-wow-delay="1.5s">
 				<div class="carousel-item active">
 					<div class="row">
 						<!-- Single Feature -->
 						<div class="col-12 col-md">
-							<div class="fplus-single-feature wow fadeInUp" data-wow-delay="1s">
+							<div class="fplus-single-feature">
 								<div class="feature-title d-flex align-items-center">
 									<img src="img/icons/wallet.svg" alt="">
 									<h5>Calidad garantizada</h5>
@@ -82,7 +48,7 @@
 						</div>
 						<!-- Single Feature -->
 						<div class="col-12 col-md">
-							<div class="fplus-single-feature wow fadeInUp" data-wow-delay="1.5s">
+							<div class="fplus-single-feature">
 								<div class="feature-title d-flex align-items-center">
 									<img src="img/icons/credit-card.svg" alt="">
 									<h5>Precios razonables</h5>
@@ -92,7 +58,7 @@
 						</div>
 						<!-- Single Feature -->
 						<div class="col-12 col-md">
-							<div class="fplus-single-feature wow fadeInUp" data-wow-delay="2s">
+							<div class="fplus-single-feature">
 								<div class="feature-title d-flex align-items-center">
 									<img src="img/icons/switching-user.svg" alt="">
 									<h5>Soporte contínuo</h5>
@@ -106,7 +72,7 @@
 					<div class="row">
 						<!-- Single Feature -->
 						<div class="col-12 col-md">
-							<div class="fplus-single-feature wow fadeInUp" data-wow-delay="1s">
+							<div class="fplus-single-feature">
 								<div class="feature-title d-flex align-items-center">
 									<img src="img/icons/wallet.svg" alt="">
 									<h5>Calidad garantizada</h5>
@@ -116,7 +82,41 @@
 						</div>
 						<!-- Single Feature -->
 						<div class="col-12 col-md">
-							<div class="fplus-single-feature wow fadeInUp" data-wow-delay="1.5s">
+							<div class="fplus-single-feature">
+								<div class="feature-title d-flex align-items-center">
+									<img src="img/icons/credit-card.svg" alt="">
+									<h5>Precios razonables</h5>
+								</div>
+								<p>Planes de pago que se adaptan a cualquier presupuesto, y a cualquier tipo de empresa. Trabajamos bajo sus condiciones</p>
+							</div>
+						</div>
+						<!-- Single Feature -->
+						<div class="col-12 col-md">
+							<div class="fplus-single-feature">
+								<div class="feature-title d-flex align-items-center">
+									<img src="img/icons/switching-user.svg" alt="">
+									<h5>Soporte contínuo</h5>
+								</div>
+								<p>Nuestro equipo está preparado para atender las necesidades más puntuales. Nuestro servicio fideliza a nuestro clientes</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<div class="row">
+						<!-- Single Feature -->
+						<div class="col-12 col-md">
+							<div class="fplus-single-feature">
+								<div class="feature-title d-flex align-items-center">
+									<img src="img/icons/wallet.svg" alt="">
+									<h5>Calidad garantizada</h5>
+								</div>
+								<p>Todos nuestros servicios cumplen con normas y estándares de calidad que garitizan la satisfacción de nuestro clientes</p>
+							</div>
+						</div>
+						<!-- Single Feature -->
+						<div class="col-12 col-md">
+							<div class="fplus-single-feature ">
 								<div class="feature-title d-flex align-items-center">
 									<img src="img/icons/credit-card.svg" alt="">
 									<h5>Precios razonables</h5>
@@ -143,7 +143,7 @@
 				<li data-target="#carouselCaracteristicas" data-slide-to="2" class="svg-invert "></li>
 			</ol>
 
-		</div>	
+		</div>
 
 	</div>
 </section>
