6429d1e37b7d336d1ce36903d4a74bbda25fb449
Luis Mario Ramirez Peralta <luismarioram99@gmail.com>
2020-12-14 11:39:29 -0600

    Creado componente de quienes somos

---
+++ component-show.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Componentes</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="/css/componentes.css">
+	<link rel="stylesheet" href="/css/bootstrap/bootstrap.min.css">
+</head>
+<body>
+	<?php include "componentes/quienes-somos.php" ?>
+	<style>
+		.svg-invert{
+			filter: invert(100%);
+		}
+	</style>
+	<script src="/js/jquery/jquery.min.js"></script>
+	<script src="/js/bootstrap/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ componentes/quienes-somos.php
@@ -0,0 +1,150 @@
+<link href="/css/componentes.css" rel="stylesheet">
+<!-- ****** About Us Area Start ****** -->
+<section class="fplus-about-us-area bg-gray section-padding-120" id="about">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<div class="section-heading text-center">
+					<h4>Quienes Somos</h4>
+					<div class="section-heading-line"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="about-us-content wow fadeInLeftBig" data-wow-delay="0.5">
+			<div class="row no-gutters align-items-center">
+				<div class="col-12 col-md-6">
+					<div class="about-us-thumb wow fadeIn" data-wow-delay="1s">
+						<img src="img/placeholder.jpg" alt="" style="width:100%; height:100%; object-fit:cover">
+					</div>
+				</div>
+				<div class="col-12 col-md-6">
+					<div class="about-us-text wow fadeIn" data-wow-delay="1.5s">
+						<h4>Somos Indasa</h4>
+						<p>Favor redactar información de Acerca de y una breve historia. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce enim nulla, mollis eu metus in, sagittis fringilla tortor. Phasellus eget purus id felis dignissim convallis. Suspendisse et augue dui. Morbi gravida sed justo vel venenatis. Ut tempor pretium maximus. Donec libero diam, faucibus vitae lectus nec, accumsan gravida dui. Nam interdum mi eget lacus aliquet, sit amet ultricies.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+
+		<div id="carouselCaracteristicas" class="carousel slide" data-ride="carousel">
+
+			<div class="carousel-inner">
+				<div class="carousel-item">
+					<div class="row">
+						<!-- Single Feature -->
+						<div class="col-12 col-md">
+							<div class="fplus-single-feature wow fadeInUp" data-wow-delay="1s">
+								<div class="feature-title d-flex align-items-center">
+									<img src="img/icons/wallet.svg" alt="">
+									<h5>Calidad garantizada</h5>
+								</div>
+								<p>Todos nuestros servicios cumplen con normas y estándares de calidad que garitizan la satisfacción de nuestro clientes</p>
+							</div>
+						</div>
+						<!-- Single Feature -->
+						<div class="col-12 col-md">
+							<div class="fplus-single-feature wow fadeInUp" data-wow-delay="1.5s">
+								<div class="feature-title d-flex align-items-center">
+									<img src="img/icons/credit-card.svg" alt="">
+									<h5>Precios razonables</h5>
+								</div>
+								<p>Planes de pago que se adaptan a cualquier presupuesto, y a cualquier tipo de empresa. Trabajamos bajo sus condiciones</p>
+							</div>
+						</div>
+						<!-- Single Feature -->
+						<div class="col-12 col-md">
+							<div class="fplus-single-feature wow fadeInUp" data-wow-delay="2s">
+								<div class="feature-title d-flex align-items-center">
+									<img src="img/icons/switching-user.svg" alt="">
+									<h5>Soporte contínuo</h5>
+								</div>
+								<p>Nuestro equipo está preparado para atender las necesidades más puntuales. Nuestro servicio fideliza a nuestro clientes</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="carousel-item active">
+					<div class="row">
+						<!-- Single Feature -->
+						<div class="col-12 col-md">
+							<div class="fplus-single-feature wow fadeInUp" data-wow-delay="1s">
+								<div class="feature-title d-flex align-items-center">
+									<img src="img/icons/wallet.svg" alt="">
+									<h5>Calidad garantizada</h5>
+								</div>
+								<p>Todos nuestros servicios cumplen con normas y estándares de calidad que garitizan la satisfacción de nuestro clientes</p>
+							</div>
+						</div>
+						<!-- Single Feature -->
+						<div class="col-12 col-md">
+							<div class="fplus-single-feature wow fadeInUp" data-wow-delay="1.5s">
+								<div class="feature-title d-flex align-items-center">
+									<img src="img/icons/credit-card.svg" alt="">
+									<h5>Precios razonables</h5>
+								</div>
+								<p>Planes de pago que se adaptan a cualquier presupuesto, y a cualquier tipo de empresa. Trabajamos bajo sus condiciones</p>
+							</div>
+						</div>
+						<!-- Single Feature -->
+						<div class="col-12 col-md">
+							<div class="fplus-single-feature wow fadeInUp" data-wow-delay="2s">
+								<div class="feature-title d-flex align-items-center">
+									<img src="img/icons/switching-user.svg" alt="">
+									<h5>Soporte contínuo</h5>
+								</div>
+								<p>Nuestro equipo está preparado para atender las necesidades más puntuales. Nuestro servicio fideliza a nuestro clientes</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<div class="row">
+						<!-- Single Feature -->
+						<div class="col-12 col-md">
+							<div class="fplus-single-feature wow fadeInUp" data-wow-delay="1s">
+								<div class="feature-title d-flex align-items-center">
+									<img src="img/icons/wallet.svg" alt="">
+									<h5>Calidad garantizada</h5>
+								</div>
+								<p>Todos nuestros servicios cumplen con normas y estándares de calidad que garitizan la satisfacción de nuestro clientes</p>
+							</div>
+						</div>
+						<!-- Single Feature -->
+						<div class="col-12 col-md">
+							<div class="fplus-single-feature wow fadeInUp" data-wow-delay="1.5s">
+								<div class="feature-title d-flex align-items-center">
+									<img src="img/icons/credit-card.svg" alt="">
+									<h5>Precios razonables</h5>
+								</div>
+								<p>Planes de pago que se adaptan a cualquier presupuesto, y a cualquier tipo de empresa. Trabajamos bajo sus condiciones</p>
+							</div>
+						</div>
+						<!-- Single Feature -->
+						<!-- <div class="col-12 col">
+							<div class="fplus-single-feature wow fadeInUp" data-wow-delay="2s">
+								<div class="feature-title d-flex align-items-center">
+									<img src="img/icons/switching-user.svg" alt="">
+									<h5>Soporte contínuo</h5>
+								</div>
+								<p>Nuestro equipo está preparado para atender las necesidades más puntuales. Nuestro servicio fideliza a nuestro clientes</p>
+							</div>
+						</div> -->
+					</div>
+				</div>
+			</div>
+			<ol class="carousel-indicators" style="position: relative; top:20px">
+				<li data-target="#carouselCaracteristicas" data-slide-to="0" class="svg-invert active"></li>
+				<li data-target="#carouselCaracteristicas" data-slide-to="1" class="svg-invert "></li>
+				<li data-target="#carouselCaracteristicas" data-slide-to="2" class="svg-invert "></li>
+			</ol>
+
+		</div>	
+
+	</div>
+</section>
+<!-- ****** About Us Area End ****** -->
