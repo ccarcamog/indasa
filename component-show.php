f736dae1deee90cc32bd48e65e60148323b57bdb
Luis Mario Ramirez Peralta <luismarioram99@gmail.com>
2020-12-21 18:09:03 -0600

    Añadido componente de galería

--- component-show.php
+++ component-show.php
@@ -7,24 +7,23 @@
 	<title>Componentes</title>
 	<link rel="stylesheet" href="style.css">
 	<link rel="stylesheet" href="/css/componentes.css">
-	<link rel="stylesheet" href="/css/others/simplegallery.demo1.css">
 	<link rel="stylesheet" href="/css/bootstrap/bootstrap.min.css">
 </head>
 
 <body>
 	<?php include "componentes/galeria.php" ?>
+
+
 	<!-- Jquery-2.2.4 js -->
-	<script src="js/jquery/jquery.min.js"></script>
-	<!-- GALERIA --> 
-	<script src="/js/simplegallery.min.js"></script>
+	<script src="/js/jquery/jquery.min.js"></script>
 	<!-- Popper js -->
-	<script src="js/bootstrap/popper.min.js"></script>
+	<script src="/js/bootstrap/popper.min.js"></script>
 	<!-- Bootstrap-4 js -->
-	<script src="js/bootstrap/bootstrap.min.js"></script>
+	<script src="/js/bootstrap/bootstrap.min.js"></script>
 	<!-- All Plugins js -->
-	<script src="js/others/plugins.js"></script>
+	<script src="/js/others/plugins.js"></script>
 	<!-- Active JS -->
-	<script src="js/active.js"></script>
+	<script src="/js/active.js"></script>
 </body>
 
 </html>
--- componentes/galeria.php
+++ componentes/galeria.php
@@ -1,60 +1,22 @@
-<div class="container p-5">
+<div class="container">
+	
+	<ul class="wow fadeInUp" id="thumbnails">
 
-	<div class="row">
-		<div class="col-12">
-			<div class="section-heading text-center">
-				<h4>Galeria</h4>
-				<div class="section-heading-line"></div>
-			</div>
-		</div>
+		<?php for ($i = 0; $i < 6; $i++) { ?>
+			<li>
+				<a href="#slide<?= $i + 1 ?>">
+					<img src="/img/placeholder.jpg" alt="This is caption <?= $i + 1 ?>">
+				</a>
+			</li>
+		<?php } ?>
+	</ul>
+	<div class="thumb-box">
+		<ul class="thumbs">
+			<?php for ($i = 0; $i < 6; $i++) { ?>
+				<li class="wow fadeInUp" data-wow-delay="<?=0.2*$i?>s"><a href="#<?=($i+1)?>" data-slide="<?=($i+1)?>"><img src="/img/placeholder.jpg" alt="This is caption 1 <a href='#link'>Even with links!</a>"></a></li>
+			<?php } ?>
+			
+		</ul>
 	</div>
 
-	<div id="gallery" class="simplegallery">
-		<div class="content text-center">
-
-			<div id="galeriaCarousel" class="carousel slide d-flex flex-column justify-content-center" data-ride="carousel">
-				<div class="carousel-inner">
-					<?php
-					for ($i = 0; $i < 3; $i++) {
-					?>
-						<div class="carousel-item <?php if (!$i) echo "active" ?>">
-							<img src="/img/placeholder.jpg" class="image_<?= $i + 1 ?>" style="width:400px; height:400px" />
-							<p class="caption caption_<?= $i + 1 ?>"><strong>Titulo</strong><br>Descripción</p>
-						</div>
-					<?php
-					}
-					?>
-				</div>
-				<a class="carousel-control-prev invert-color" href="#galeriaCarousel" role="button" data-slide="prev">
-					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-					<span class="sr-only ">Previous</span>
-				</a>
-				<a class="carousel-control-next invert-color" href="#galeriaCarousel" role="button" data-slide="next">
-					<span class="carousel-control-next-icon" aria-hidden="true"></span>
-					<span class="sr-only">Next</span>
-				</a>
-			</div>
-
-
-		</div>
-
-		<div class="clear"></div>
-
-		<div class="thumbnail container">
-			<?php
-			for ($i = 0; $i < 3; $i++) {
-			?>
-				<div class="thumb" data-id="<?= $i ?>" data-galeria="#galeriaCarousel">
-
-					<img src="/img/placeholder.jpg" id="thumb_<?= ($i + 1) ?>" class="thumbs" alt="" />
-
-				</div>
-			<?php
-			}
-			?>
-
-
-		</div>
-
-	</div>
 </div>
