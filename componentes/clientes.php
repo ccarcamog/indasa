850b81a2083396987516afdddbf232b44f109735
Luis Mario Ramirez Peralta <luismarioram99@gmail.com>
2020-12-21 21:07:24 -0600

    Añadido componente de clientes

---
+++ componentes/clientes.php
@@ -0,0 +1,61 @@
+<section class="fplus-projects-area bg-gray section-padding-0-120 mt-5" id="projects">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<div class="section-heading text-center">
+					<h4>Clientes</h4>
+					<div class="section-heading-line"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid">
+		<div class="row no-gutters fplus-portfolio">
+			<!-- Single gallery Item Start -->
+			<div class="col-12 col-sm-6 col-md-3 single_gallery_item branding wow fadeInUp" data-wow-delay="0.2s">
+				<img src="img/New-Breed.png" alt="">
+				<div class="gallery-hover-overlay d-flex justify-content-between">
+					<div class="port-more-view">
+					</div>
+					<div class="port-hover-text">
+						<h3>New Breed</h3>
+					</div>
+				</div>
+			</div>
+			<!-- Single gallery Item Start -->
+			<div class="col-12 col-sm-6 col-md-3 single_gallery_item branding wow fadeInUp" data-wow-delay="0.4s">
+				<img src="img/Grupo-AIDER-Logo.png" alt="">
+				<div class="gallery-hover-overlay d-flex justify-content-between">
+					<div class="port-more-view">
+					</div>
+					<div class="port-hover-text">
+						<h3>Grupo AIDER</h3>
+					</div>
+				</div>
+			</div>
+			<!-- Single gallery Item Start -->
+			<div class="col-12 col-sm-6 col-md-3 single_gallery_item design wow fadeInUp" data-wow-delay="0.6s">
+				<img src="img/seman_logo.jpeg" alt="">
+				<div class="gallery-hover-overlay d-flex justify-content-between">
+					<div class="port-more-view">
+					</div>
+					<div class="port-hover-text">
+						<h3>SERMAN</h3>
+					</div>
+				</div>
+			</div>
+			<!-- Single gallery Item Start -->
+			<div class="col-12 col-sm-6 col-md-3 single_gallery_item graphic wow fadeInUp" data-wow-delay="0.8s">
+				<img src="img/wa.jpg" alt="">
+				<div class="gallery-hover-overlay d-flex justify-content-between">
+					<div class="port-more-view">
+					</div>
+					<div class="port-hover-text">
+						<h3>W Asociados</h3>
+					</div>
+				</div>
+			</div>
+
+		</div>
+</section>
--- componentes/galeria.php
+++ componentes/galeria.php
@@ -1,5 +1,15 @@
-<div class="container">
-	
+<div class="container mt-5">
+
+	<div class="row">
+		<div class="col-12">
+			<div class="section-heading text-center">
+				<h4>Calería</h4>
+				<div class="section-heading-line"></div>
+			</div>
+		</div>
+	</div>
+
+
 	<ul class="wow fadeInUp" id="thumbnails">
 
 		<?php for ($i = 0; $i < 6; $i++) { ?>
@@ -13,9 +23,9 @@
 	<div class="thumb-box">
 		<ul class="thumbs">
 			<?php for ($i = 0; $i < 6; $i++) { ?>
-				<li class="wow fadeInUp" data-wow-delay="<?=0.2*$i?>s"><a href="#<?=($i+1)?>" data-slide="<?=($i+1)?>"><img src="/img/placeholder.jpg" alt="This is caption 1 <a href='#link'>Even with links!</a>"></a></li>
+				<li class="wow fadeInUp" data-wow-delay="<?= 0.2 * $i ?>s"><a href="#<?= ($i + 1) ?>" data-slide="<?= ($i + 1) ?>"><img src="/img/placeholder.jpg" alt="This is caption 1 <a href='#link'>Even with links!</a>"></a></li>
 			<?php } ?>
-			
+
 		</ul>
 	</div>
 
