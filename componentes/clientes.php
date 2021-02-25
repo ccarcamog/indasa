59c774dce5af2d609a9865e1f51a49ba82b88e9f
Luis Mario Ramirez Peralta <luismarioram99@gmail.com>
2021-02-25 17:25:57 -0600

    Cambiadas img de clientes y marcas y cambios estéticos en component-show

--- componentes/clientes.php
+++ componentes/clientes.php
@@ -14,36 +14,22 @@
 		<div class="row fplus-portfolio">
 			<!-- Single gallery Item Start -->
 			<div class="col-12 col-sm-6 col-md-4 single_gallery_item branding wow fadeInUp" data-wow-delay="0.2s">
-				<img src="img/grupo-aider/Logo_Corporativo.png" alt="">
-				<div class="gallery-hover-overlay d-flex justify-content-between">
+				<img src="img/grupo-aider/Logos svg aider cl-01.svg" alt="">
+				<!-- <div class="gallery-hover-overlay d-flex justify-content-between">
 					<div class="port-more-view">
 					</div>
 					<div class="port-hover-text">
 						<h3>Universidad Francisco Gavidia</h3>
 					</div>
-				</div>
+				</div> -->
 			</div>
 			<!-- Single gallery Item Start -->
 			<div class="col-12 col-sm-6 col-md-4 single_gallery_item branding wow fadeInUp" data-wow-delay="0.4s">
-				<img src="img/grupo-aider/LOGO-HOSPITAL-MONTE-ESPAÑA-PNG_2.png" alt="">
-				<div class="gallery-hover-overlay d-flex justify-content-between">
-					<div class="port-more-view">
-					</div>
-					<div class="port-hover-text">
-						<h3>Hospital Monte España</h3>
-					</div>
-				</div>
+				<img src="img/grupo-aider/Logos svg aider cl-03.svg" alt="">
 			</div>
 			<!-- Single gallery Item Start -->
 			<div class="col-12 col-sm-6 col-md-4 single_gallery_item design wow fadeInUp" data-wow-delay="0.6s">
-				<img src="img/grupo-aider/logo_2caedbaea2185ed52d43a65ecb62a8d6_1x.png" alt="">
-				<div class="gallery-hover-overlay d-flex justify-content-between">
-					<div class="port-more-view">
-					</div>
-					<div class="port-hover-text">
-						<h3>Hielo Polar</h3>
-					</div>
-				</div>
+				<img src="img/grupo-aider/Logos svg aider cl-02.svg" alt="">
 			</div>
 		
 
--- componentes/marcas.php
+++ componentes/marcas.php
@@ -10,71 +10,26 @@
 		</div>
 	</div>
 
-	<div class="container-fluid">
+	<div class="container">
+
 		<div class="row fplus-portfolio">
-			<!-- Single gallery Item Start -->
-			<div class="col-12 col-sm-6 col-md-4 single_gallery_item branding wow fadeInUp" data-wow-delay="0.2s">
-				<img src="img/grupo-aider/canadian-solar-logo.png" alt="">
-				<div class="gallery-hover-overlay d-flex justify-content-between">
+			<?php
+			for ($i = 0; $i < 9; $i++) {
+			?>
+				<!-- Single gallery Item Start -->
+				<div class="col-12 col-sm-6 col-md-4 single_gallery_item branding wow fadeInUp" data-wow-delay="0.2s">
+					<img src="img/grupo-aider/logo-marca-<?= $i + 1 ?>.svg" alt="">
+					<!-- <div class="gallery-hover-overlay d-flex justify-content-between">
 					<div class="port-more-view">
 					</div>
 					<div class="port-hover-text">
 						<h3>Canadian Solar</h3>
 					</div>
+				</div> -->
 				</div>
-			</div>
-			<!-- Single gallery Item Start -->
-			<div class="col-12 col-sm-6 col-md-4 single_gallery_item branding wow fadeInUp" data-wow-delay="0.4s">
-				<img src="img/grupo-aider/1200px-Jinko_Solar_logo.svg.png" alt="">
-				<div class="gallery-hover-overlay d-flex justify-content-between">
-					<div class="port-more-view">
-					</div>
-					<div class="port-hover-text">
-						<h3>Jinko Solar</h3>
-					</div>
-				</div>
-			</div>
-			<!-- Single gallery Item Start -->
-			<div class="col-12 col-sm-6 col-md-4 single_gallery_item design wow fadeInUp" data-wow-delay="0.6s">
-				<img src="img/grupo-aider/Logo_SMA.svg.png" alt="">
-				<div class="gallery-hover-overlay d-flex justify-content-between">
-					<div class="port-more-view">
-					</div>
-					<div class="port-hover-text">
-						<h3>SMA</h3>
-					</div>
-				</div>
-			</div>
-			<div class="col-12 col-sm-6 col-md-4 single_gallery_item design wow fadeInUp" data-wow-delay="0.6s">
-				<img src="img/grupo-aider/fronius-1.svg" alt="">
-				<div class="gallery-hover-overlay d-flex justify-content-between">
-					<div class="port-more-view">
-					</div>
-					<div class="port-hover-text">
-						<h3>Fronius</h3>
-					</div>
-				</div>
-			</div>
-			<div class="col-12 col-sm-6 col-md-4 single_gallery_item design wow fadeInUp" data-wow-delay="0.6s">
-				<img src="img/grupo-aider/Sonnen_logo.png" alt="">
-				<div class="gallery-hover-overlay d-flex justify-content-between">
-					<div class="port-more-view">
-					</div>
-					<div class="port-hover-text">
-						<h3>Sonnen</h3>
-					</div>
-				</div>
-			</div>
-			<div class="col-12 col-sm-6 col-md-4 single_gallery_item design wow fadeInUp" data-wow-delay="0.6s">
-				<img src="img/grupo-aider/ironridge.png" alt="">
-				<div class="gallery-hover-overlay d-flex justify-content-between">
-					<div class="port-more-view">
-					</div>
-					<div class="port-hover-text">
-						<h3>Iron Ridge</h3>
-					</div>
-				</div>
-			</div>
 
+			<?php
+			}
+			?>
 		</div>
 </section>
