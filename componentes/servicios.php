7671b4d5e68071d9f5f3fb70b6cb40e7666aafa0
Luis Mario Ramirez Peralta <luismarioram99@gmail.com>
2021-03-04 16:48:36 -0600

    Actualizados servicios e imágenes

--- componentes/servicios.php
+++ componentes/servicios.php
@@ -1,56 +1,123 @@
-
-    <!-- ****** Blog Area Start ****** --->
-    <section class="fplus-blog-area bg-gray section-padding-0-120" id="services">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="section-heading text-center">
-                        <h4>Servicios</h4>
-                        <div class="section-heading-line"></div>
-                    </div>
+<!-- ****** Blog Area Start ****** --->
+<section class="fplus-blog-area bg-gray section-padding-0-120" id="services">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="section-heading text-center">
+                    <h4>Servicios</h4>
+                    <div class="section-heading-line"></div>
                 </div>
-            </div>
-
-            <div class="row">
-                <!-- Single Blog Post Area -->
-                <div class="col-12 col-md-6 col-lg-4">
-                    <div class="fplus-single-blog-area wow fadeInUp" data-wow-delay="0.5s">
-                        <!-- Blog Thumbnail -->
-                        <img src="img/grupo-aider/129733675_2842507236026675_1859087375636870366_o.jpg" alt="" style="width:100%; height:200px; object-fit: cover">
-                        <!-- Blog Content  -->
-                        <div class="fplus-blog-content">
-							<h5>Diseño y Construcción</h5>
-							<p class="text-muted">Evaluación, diseño y puesta en marcha de proyectos de energías renovables bajo el esquema de 
-								llave en mano. </p>
-                            
-                        </div>
-                    </div>
-                </div>
-                <!-- Single Blog Post Area  -->
-                <div class="col-12 col-md-6 col-lg-4">
-                    <div class="fplus-single-blog-area wow fadeInUp" data-wow-delay="1s">
-                        <!-- Blog Thumbnail  -->
-                        <img src="img/grupo-aider/pexels-tiger-lily-4484155.jpg" alt="" style="width:100%; height:200px; object-fit: cover">
-                        <!-- Blog Content  -->
-                        <div class="fplus-blog-content">
-						<h5>Equipo de primera calidad</h5>
-						<p class="text-muted">Suministro de equipos y materiales para proyectos de energías renovables. </p>
-                        </div>
-                    </div>
-                </div>
-                <!-- Single Blog Post Area  -->
-                <div class="col-12 col-lg-4">
-                    <div class="fplus-single-blog-area wow fadeInUp" data-wow-delay="1.5s">
-                        <!-- Blog Thumbnail  -->
-                        <img src="img/grupo-aider/pexels-pixabay-159397.jpg" alt="" style="width:100%; height:200px; object-fit: cover">
-                        <!-- Blog Content  -->
-                        <div class="fplus-blog-content"> 
-							<h5>Operación y Mantenimiento</h5>
-							<p class="text-muted">Instalación mecánica y eléctrica de proyectos de energías renovables.</p>
-                        </div>
-                    </div>
-				</div>
             </div>
         </div>
-    </section>
-    <!-- ****** Blog Area End ****** -->
+        <div id="carouselServicios" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner wow fadeInUp" data-wow-delay="1.5s">
+                <div class="carousel-item active">
+                    <div class="row">
+                        <!-- Single Blog Post Area -->
+                        <div class="col-12 col-md-6 col-lg-4">
+                            <div class="fplus-single-blog-area">
+                                <!-- Blog Thumbnail -->
+                                <img src="img/grupo-aider/129733675_2842507236026675_1859087375636870366_o.jpg" alt="" style="width:100%; height:200px; object-fit: cover">
+                                <!-- Blog Content  -->
+                                <div class="fplus-blog-content">
+                                    <h5>Diseño y Construcción</h5>
+                                    <p class="text-muted">Evaluación, diseño y puesta en marcha de proyectos de energías renovables bajo el esquema de
+                                        llave en mano. </p>
+
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Single Blog Post Area  -->
+                        <div class="col-12 col-md-6 col-lg-4">
+                            <div class="fplus-single-blog-area wow">
+                                <!-- Blog Thumbnail  -->
+                                <img src="img/grupo-aider/pexels-tiger-lily-4484155.jpg" alt="" style="width:100%; height:200px; object-fit: cover">
+                                <!-- Blog Content  -->
+                                <div class="fplus-blog-content">
+                                    <h5>Equipo de primera calidad</h5>
+                                    <p class="text-muted">Suministro de equipos y materiales para proyectos de energías renovables. </p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Single Blog Post Area  -->
+                        <div class="col-12 col-lg-4">
+                            <div class="fplus-single-blog-area wow">
+                                <!-- Blog Thumbnail  -->
+                                <img src="img/grupo-aider/pexels-pixabay-159397.jpg" alt="" style="width:100%; height:200px; object-fit: cover">
+                                <!-- Blog Content  -->
+                                <div class="fplus-blog-content">
+                                    <h5>Operación y Mantenimiento</h5>
+                                    <p class="text-muted">Instalación mecánica y eléctrica de proyectos de energías renovables.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-12 col-md-6 col-lg-4">
+                            <div class="fplus-single-blog-area ">
+                                <!-- Blog Thumbnail -->
+                                <img src="img/grupo-aider/pexels-burak-k-186461.jpg" alt="" style="width:100%; height:200px; object-fit: cover">
+                                <!-- Blog Content  -->
+                                <div class="fplus-blog-content">
+                                    <h5>Análisis Técnico y Económico</h5>
+                                    <p class="text-muted">Estudios de energía y estructurales para el desarrollo de proyectos.</p>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <ol class="carousel-indicators" style="position: relative; top:20px">
+                <li data-target="#carouselServicios" data-slide-to="0" class="invert-color active"></li>
+                <li data-target="#carouselServicios" data-slide-to="1" class="invert-color "></li>
+
+            </ol>
+        </div>
+
+    </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-12">
+                <div class="m-0 text-center text-muted">
+                    <h5>Instalaciones para múltiples propósitos</h5>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <!-- Single Feature -->
+            <div class="col-12 col-md">
+                <div class="fplus-single-feature">
+                    <div class="feature-title d-flex align-items-center">
+                        <img src="img/icons/factory.svg" alt="">
+                        <h5>Industrial</h5>
+                    </div>
+                    <p>Energías renovables en alta escala. Retorno de inversión rápido y eficiente.</p>
+                </div>
+            </div>
+            <!-- Single Feature -->
+            <div class="col-12 col-md">
+                <div class="fplus-single-feature">
+                    <div class="feature-title d-flex align-items-center">
+                        <img src="img/icons/tienda.svg" alt="">
+                        <h5>Comercial</h5>
+                    </div>
+                    <p>Llevamos energías limpias a tu negocio para un mudo mejor</p>
+                </div>
+            </div>
+            <!-- Single Feature -->
+            <div class="col-12 col-md">
+                <div class="fplus-single-feature">
+                    <div class="feature-title d-flex align-items-center">
+                        <img src="img/icons/smart-home.svg" alt="">
+                        <h5>Residencial</h5>
+                    </div>
+                    <p>Energía solar para tu hogar. Ahorra y cuida el medio ambiente</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ****** Blog Area End ****** -->
